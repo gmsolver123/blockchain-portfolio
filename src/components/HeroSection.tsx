4ed9fe3354ad866b215d6ff1afc5582a378af567
gmsolver123 <gmsolver123@gmail.com>
2025-11-17 06:51:41 -0500

    fix buttons responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -46,17 +46,17 @@ const HeroSection = () => {
             <div className="flex flex-col sm:flex-row gap-4">
               <button 
                 onClick={() => document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })}
-                className="btn-cyber group"
+                className="btn-cyber group flex items-center whitespace-nowrap"
               >
-                Explore My Work
-                <ArrowRight className="w-5 h-5 ml-2 transition-transform group-hover:translate-x-1" />
+                <span>Explore My Work</span>
+                <ArrowRight className="w-5 h-5 ml-2 transition-transform group-hover:translate-x-1 flex-shrink-0" />
               </button>
               <button 
                 onClick={() => document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' })}
-                className="btn-ghost group"
+                className="btn-ghost group flex items-center whitespace-nowrap"
               >
-                <Zap className="w-5 h-5 mr-2" />
-                Get in Touch
+                <Zap className="w-5 h-5 mr-2 flex-shrink-0" />
+                <span>Get in Touch</span>
               </button>
             </div>
 
